934b534d7b469c383ff1e55f184231541bf352f8
cesarperez1908 <34453077+cesarperez1908@users.noreply.github.com>
2017-12-12 16:13:31 -0500

    Update botonarriba.js

--- botonarriba.js
+++ botonarriba.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
  
 	$('.home').click(function(){
 		$('body, html').animate({
-			scrollTop: '100px'
+			scrollTop: '200vw'
 		}, 2000);
 	});
  
 	$(window).scroll(function(){
-		if( $(this).scrollTop() > 100 ){
+		if( $(this).scrollTop() > 200 ){
 			$('.home').slideDown(2000);
 		} else {
 			$('.home').slideUp(2000);
